02dabf21f9a58895fe8e5e0829f02f7c4a5b1192
Oscar Holmér <weep.133@gmail.com>
2017-10-24 20:30:59 +0200

    added jqyery

--- EveHelper.Web/src/app/login/login.component.ts
+++ EveHelper.Web/src/app/login/login.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  private vCode: string;
+  private keyId: string;
 
-  constructor() { }
+  constructor() {
+    var authString = window.localStorage.getItem("auth");
+    console.log(authString);
+    var auth = JSON.parse(authString) || {};
+    console.log(auth);
+    this.vCode = auth.vCode;
+    this.keyId = auth.keyId;
+  }
 
   ngOnInit() {
+  }
+
+  saveCode() {
+    console.log(this);
+
+    var auth = {
+      "vCode": this.vCode,
+      "keyId": this.keyId
+    };
+    var authString = JSON.stringify(auth);
+    console.log(auth, authString);
+    window.localStorage.setItem("auth", authString);
+    console.log("saved");
   }
 
 }
