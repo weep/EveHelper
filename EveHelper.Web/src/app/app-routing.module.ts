77c66f2d01a866ac9cc3251d8e2fbc0c0fc69a12
Oscar Holmér <weep.133@gmail.com>
2017-10-24 19:33:41 +0200

    added some routes

--- EveHelper.Web/src/app/app-routing.module.ts
+++ EveHelper.Web/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+}, {
+  path: 'login',
+  component: LoginComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
