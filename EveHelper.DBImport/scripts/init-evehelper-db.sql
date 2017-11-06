33000b339ce12a0cd95f3a012bb496846a8454a3
Oscar Holmér <weep.133@gmail.com>
2017-11-06 09:49:59 +0100

    restore database dump

--- EveHelper.DBImport/scripts/init-evehelper-db.sql
+++ EveHelper.DBImport/scripts/init-evehelper-db.sql
@@ -2,10 +2,4 @@ CREATE USER evehelper;
 CREATE DATABASE evehelper;
 GRANT ALL PRIVILEGES ON DATABASE evehelper TO evehelper;
 
---CREATE ROLE yaml;
---GRANT ALL PRIVILEGES ON DATABASE evehelper TO yaml;
-
-
-
-
---exec pg_restore -d evehelper -F c /app/postgres-latest.dmp;
+CREATE SCHEMA evesde;
--- EveHelper.DBImport/scripts/loaddb.sh
+++ EveHelper.DBImport/scripts/loaddb.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-#    CREATE USER docker;
-#    CREATE DATABASE docker;
-#    GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
-#EOSQL
+#wget https://www.fuzzwork.co.uk/dump/postgres-latest.dmp.bz2
+#bzip2 -d postgres-latest.dmp.bz2
 
-#psql evehelper -f /app/postgres-latest.dmp
-
-#pg_restore -d evehelper -a /app/postgres-latest.dmp
-#
-#psql -f globals.sql
-#psql -f db-schema.sql evehelper
+##wget https://www.fuzzwork.co.uk/dump/postgres-schema-latest.dmp.bz2
+bzip2 -d postgres-schema-latest.dmp.bz2
 
 pg_restore --no-privileges --no-owner -d evehelper -c /app/postgres-schema-latest.dmp
-pg_restore --no-privileges --no-owner -d evehelper -c /app/postgres-latest.dmp
+#pg_restore --no-privileges --no-owner -d evehelper -c /app/postgres-latest.dmp
